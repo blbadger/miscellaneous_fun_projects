04a6b5fc916d872832ae795173ece2907ec69626
blbadger <blbadger10@gmail.com>
2021-05-27 10:07:48 -0400

    refactored sudoku

--- sudoku_solver1.py
+++ sudoku_solver1.py
@@ -1,5 +1,5 @@
 def solve(puzzle):
-	'''A function that takes a list of lists denoting sudoku
+	'''A function that takes a list of lists (9x9) denoting sudoku
 	puzzle clues and places to guess (0s) and returns the completed
 	puzzle as a list of lists. Expects a solveable puzzle, and will 
 	return only solution if multiple exist.
@@ -12,54 +12,57 @@ def solve(puzzle):
 			if puzzle[x][y] == 0:
 				ls.append([x,y])
 
-	# backtracking algorithm with validation tests
-	def backtrack(puzzle, ls):
-		'''Solves the sudoku puzzle by iterating through the 
-		positions in ls and entering possible values into 
-		the puzzle array.  Backtracking occurs when no entry
-		is possible for a given space.
-		'''
-		i = 0
-		while i in range(len(ls)):
-			a, b = ls[i]
-			count = 0
+	# backtrack for each position with validation tests
+	return backtrack(puzzle, ls)
 
-			if puzzle[a][b] == 0: 
-				c = 0
 
-			else: 
-				c = puzzle[a][b]
+def backtrack(puzzle, ls):
+	'''Solves the sudoku puzzle by iterating through the 
+	positions in ls and entering possible values into 
+	the puzzle array.  Backtracking occurs when no entry
+	is possible for a given space.  
+	'''
+	i = 0
+	while i in range(len(ls)):
+		a, b = ls[i]
+		count = 0
 
-			c += 1
-			while c < 10:
-				if c not in puzzle[a]:
-					ls2 = []
-					for q in range(9):
-						ls2.append(puzzle[q][b])
-		
-					if c not in ls2:
-						ls3 = []
-						x, y = a // 3, b // 3
-						for k in range(3*x, 3*x+3):
-							for l in range(3*y, 3*y+3):
-								ls3.append(puzzle[k][l])
+		if puzzle[a][b] == 0: 
+			c = 0
 
-						if c not in ls3:
-							puzzle[a][b] = c
-							count += 1
-							i += 1
-							break
+		else: 
+			c = puzzle[a][b]
 
-						else: c += 1
+		c += 1
+		while c < 10:
+			if c not in puzzle[a]:
+				ls2 = []
+				for q in range(9):
+					ls2.append(puzzle[q][b])
+	
+				if c not in ls2:
+					ls3 = []
+					x, y = a // 3, b // 3
+					for k in range(3*x, 3*x+3):
+						for l in range(3*y, 3*y+3):
+							ls3.append(puzzle[k][l])
+
+					if c not in ls3:
+						puzzle[a][b] = c
+						count += 1
+						i += 1
+						break
+
 					else: c += 1
 				else: c += 1
+			else: c += 1
 
-			if count == 0:
-				puzzle[a][b] = 0
-				i -= 1
-		return puzzle
+		if count == 0:
+			puzzle[a][b] = 0
+			i -= 1
+	return puzzle
+
 	
-	return backtrack(puzzle, ls)
 
 # example of an input
 puzzle = [
